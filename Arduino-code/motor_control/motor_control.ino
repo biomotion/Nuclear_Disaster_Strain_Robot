09fde2a86d3031c431b444619ef7815916d5abdd
biomotion <bensonpuppy1997@gmail.com>
2019-05-13 02:43:50 +0800

    can detect motor speed and count the angular speed

--- Arduino-code/motor_control/motor_control.ino
+++ Arduino-code/motor_control/motor_control.ino
@@ -1,11 +1,16 @@
 #define L_MOTOR_INT 2 
 #define R_MOTOR_INT 3
 
+// 2pi = 980 triggered times
+const double rot_per_count = 2*M_PI/980;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(L_MOTOR_INT, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(L_MOTOR_INT), left_int, RISING);
-  
+  attachInterrupt(digitalPinToInterrupt(R_MOTOR_INT), right_int, RISING);
+  Serial.begin(115200);
+  Serial.println("Start");
 }
 
 void loop() {
@@ -14,5 +19,33 @@ void loop() {
 }
 
 void left_int(){
-  Serial.println("Left int");
+  static unsigned long prev_time = 0, curr_time = 0;
+  double left_speed_fb = 0;
+  curr_time = micros();
+  //Serial.println("Left int");
+  //Serial.print("time = ");
+  //Serial.println(curr_time);
+  if(prev_time == 0){
+    prev_time = curr_time;
+    return;
+  }
+  left_speed_fb = rot_per_count * 1e6 / (curr_time - prev_time);
+  Serial.println(left_speed_fb);
+  prev_time = curr_time;
+}
+
+void right_int(){
+  static unsigned long prev_time = 0, curr_time = 0;
+  double right_speed_fb = 0;
+  curr_time = micros();
+  //Serial.println("Left int");
+  //Serial.print("time = ");
+  //Serial.println(curr_time);
+  if(prev_time == 0){
+    prev_time = curr_time;
+    return;
+  }
+  right_speed_fb = rot_per_count * 1e6 / (curr_time - prev_time);
+  Serial.println(right_speed_fb);
+  prev_time = curr_time;
 }
