c2c8827f52acc4ed1027e3bcdb1063bf3147c56e
biomotion <bensonpuppy1997@gmail.com>
2019-05-17 02:38:46 +0800

    add pub function

--- Arduino-code/pubSubTest/pubSubTest.ino
+++ Arduino-code/pubSubTest/pubSubTest.ino
@@ -6,15 +6,17 @@
 ros::NodeHandle nh;
 
 void car_cmd_cb(const duckietown_msgs::Twist2DStamped& msg);
-
+std_msgs::String str_msg;
 //
-ros::Subscriber<std_msgs::Empty> sub_car_cmd("car_cmd_cb", car_cmd_cb );
+ros::Subscriber<duckietown_msgs::Twist2DStamped> sub_car_cmd("car_cmd_cb", car_cmd_cb);
+ros::Publisher pub_string("arduino_out", &str_msg);
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(13, OUTPUT);
   nh.initNode();
   nh.subscribe(sub_car_cmd);
+  nh.advertise(pub_string);
 }
 
 void loop() {
@@ -27,4 +29,6 @@ void car_cmd_cb(const duckietown_msgs::Twist2DStamped& msg){
   digitalWrite(13, HIGH-digitalRead(13));
   float _v = msg.v;
   float _omega = msg.omega;
+  str_msg.data = "car cmd recieved";
+  pub_string.publish(&str_msg);
 }
